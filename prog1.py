459e8ead206fbcd89ac2e848c74ae28f96db89f1
garyzhen704 <gzhen@csu.fullerton.edu>
2025-11-16 16:12:22 -0800

    finished synax analyzer

--- prog1.py
+++ prog1.py
@@ -32,24 +32,48 @@ NON_TERMINALS_SET = {'E', 'Q', 'T', 'R', 'F'}
 def parse(input_string):
     stack = ['$', 'E']
     input_index = 0
-    lookahead = input_string[input_index]
-    while stack[-1] != '$' or lookahead != '$':
-        print('stack:', stack)
-        print('lookahead: ', lookahead)
-        if stack[-1] == lookahead:
+    if input_index < (len(input_string)):
+        lookahead = input_string[input_index]
+    else:
+        lookahead = '$'
+    while True:
+        if stack[-1] == '$' and lookahead == '$':
+            print('stack:', stack)
+            #print('lookahead: ', lookahead)
+            print('String is accepted/ valid.')
+            return True
+        elif stack[-1] == lookahead:
+            print('stack:', stack)
+            #print('lookahead: ', lookahead)
+            
             stack.pop()
             input_index += 1
             
-            lookahead = input_string[input_index]
+            try:
+                lookahead = input_string[input_index]
+            except IndexError: 
+                lookahead = '$'
+                    
         elif stack[-1] in NON_TERMINALS_SET:
+            print('stack:', stack)
+            #print('lookahead: ', lookahead)
+            
             non_term = stack.pop()
-            rule_rhs = parsing_table[non_term][lookahead]
-            print('nonterm: ', non_term)
-            print('rulerhs: ', rule_rhs)
-            stack.extend(reversed(rule_rhs))
+            rule_rhs = parsing_table[non_term].get(lookahead, None)
+            
+            #print('nonterm: ', non_term)
+            #Sprint('rulerhs: ', rule_rhs)
+            
+            if rule_rhs is None:
+                print('stack: ', stack)
+                return print("String is not accepted/ invalid.")
+            elif rule_rhs == 'ε':
+                pass
+            else:
+                stack.extend(reversed(rule_rhs))
         else:
+            print('String is not accepted/ invalid.')
             return False
-    return
             
             
             
@@ -58,7 +82,10 @@ def parse(input_string):
 
 if __name__ == "__main__":
     
-    print(parsing_table['F']['a'])
+    #print(parsing_table['F']['a'])
     
     inpstring = input("inputstring: ")
+    if inpstring and inpstring[-1] != '$':
+        inpstring += '$'
+        
     parse(inpstring)
